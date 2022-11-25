dc2ed13bbdc3077f17b6d99918e97936c812f6cb
isaac-ch0i <116502636+isaac-ch0i@users.noreply.github.com>
2022-11-25 15:49:00 +0900

    Update 7_Buff.h

--- 7_Buff.h
+++ 7_Buff.h
@@ -23,10 +23,12 @@ int buff(char action[6]) {
 		if (buff_sort == 1) {
 			degree = rolling();
 			alpha.op = alpha.op + degree;
+			cout << "att_up : " << degree << endl;
 		}
 		if (buff_sort == 2) {
 			degree = rolling();
 			alpha.hp = alpha.hp + degree;
+			cout << "hp_up : " << degree << endl;
 			if (alpha.hp > 10) {
 				alpha.hp = 10;
 			}
@@ -34,14 +36,16 @@ int buff(char action[6]) {
 		if (buff_sort == 3) {
 			degree = rolling();
 			alpha.hp = alpha.hp - degree;
+			cout << "hp_down : " << degree << endl;
 		}
 	}
+	fight(action);
 	return (0);
 }
 
 int rolling() {
 	std::random_device random;
 	std::mt19937 dice(random());
-	std::uniform_int_distribution<int> roll(1, 4);
+	std::uniform_int_distribution<int> roll(1, 3);
 	return(roll(dice));
 }
